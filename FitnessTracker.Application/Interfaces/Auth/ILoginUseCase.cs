c24dc49cd2325fc1ddf5a6ef4cb5bfc97a484773
Dimonlimon2109 <dimahatcenok@gmail.com>
2025-08-01 20:46:58 +0300

    feat: add loginUseCase

---
+++ FitnessTracker.Application/Interfaces/Auth/ILoginUseCase.cs
@@ -0,0 +1,10 @@
+﻿using FitnessTracker.Application.Contracts.Requests;
+using FitnessTracker.Application.Contracts.Responses;
+
+namespace FitnessTracker.Application.Interfaces.Auth
+{
+    public interface ILoginUseCase
+    {
+        Task<LoginResponse> ExecuteAsync(LoginRequest loginRequest, CancellationToken ct = default);
+    }
+}
--- FitnessTracker.Application/UseCases/Auth/LoginUseCase.cs
+++ FitnessTracker.Application/UseCases/Auth/LoginUseCase.cs
@@ -1,23 +1,58 @@
 ﻿
 using FitnessTracker.Application.Contracts.Requests;
 using FitnessTracker.Application.Contracts.Responses;
+using FitnessTracker.Application.Interfaces.Auth;
 using FitnessTracker.Domain.Interfaces.Repositories;
 using FitnessTracker.Domain.Interfaces.Services;
 
 namespace FitnessTracker.Application.UseCases.Auth
 {
-    public class LoginUseCase
+    public class LoginUseCase : ILoginUseCase
     {
         private readonly IUserRepository _userRepository;
         private readonly IPasswordService _passwordService;
+        private readonly ITokensService _tokensService;
 
         public LoginUseCase(
             IUserRepository userRepository,
-            IPasswordService passwordService
+            IPasswordService passwordService,
+            ITokensService tokensService
             )
         {
             _userRepository = userRepository;
             _passwordService = passwordService;
+            _tokensService = tokensService;
+        }
+
+        public async Task<LoginResponse> ExecuteAsync(
+            LoginRequest loginRequest,
+            CancellationToken ct = default
+            )
+        {
+            var user = await _userRepository.GetUserByEmailAsync(loginRequest.Email);
+
+            if (user == null)
+            {
+                throw new NullReferenceException("Пользователь не найден");
+            }
+
+            if (!_passwordService.ValidatePassword(loginRequest.Password, user.PasswordHash))
+            {
+                throw new ArgumentException("Неверный пароль");
+            }
+
+            var accessToken = _tokensService.GenerateAccessToken(user);
+            var refreshTokenModel = _tokensService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshTokenModel.RefreshToken;
+            user.RefreshTokenExpiresAt = refreshTokenModel.ExpiresAt;
+
+            _userRepository.Update(user);
+            await _userRepository.SaveChangesAsync(ct);
+
+            var loginResponse = new LoginResponse(accessToken, refreshTokenModel.RefreshToken);
+
+            return loginResponse;
         }
     }
 }
