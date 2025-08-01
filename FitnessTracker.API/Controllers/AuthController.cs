9cc2e85be7881699cbdb291ca78ecbd4bb8b10ba
Dimonlimon2109 <dimahatcenok@gmail.com>
2025-08-01 16:14:13 +0300

    feat: add auth controller with register

---
+++ FitnessTracker.API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using FitnessTracker.Application.Contracts.Requests;
+using FitnessTracker.Application.Interfaces.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessTracker.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IRegisterUseCase _registerUseCase;
+
+        public AuthController(IRegisterUseCase registerUseCase)
+        {
+            _registerUseCase = registerUseCase;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(
+            RegisterRequest registerRequest,
+            CancellationToken ct = default
+            )
+        {
+            await _registerUseCase.ExecuteAsync(registerRequest, ct);
+
+            return Created();
+        }
+    }
+}
